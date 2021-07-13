5c2736d339b97e313f907b2c1fd0ad03f042e649
julproh <cherry5005@mail.ru>
2021-07-13 22:12:50 +0300

    Modified Bones

--- NetCracker/Java_Basics_and_OOP/first_task/bones/Bones.java
+++ NetCracker/Java_Basics_and_OOP/first_task/bones/Bones.java
@@ -3,9 +3,9 @@ package bones;
 import java.util.Random;
 import java.util.Scanner;
 
-import org.graalvm.compiler.hotspot.nodes.profiling.RandomSeedNode;
 
 public class Bones {
+
     public static void main (String[] args) {
 
         int n, k;
@@ -15,19 +15,54 @@ public class Bones {
         Scanner in = new Scanner(System.in);
         n = in.nextInt();
 
-        System.out.println("Print the number of the kosti");
+        System.out.println("Print the number of the bones");
         k = in.nextInt();
         in.close();
     
-        int[] players = new int[n];
-        
-        int number = 0;
+        int[][] players = new int[2][n];
+        for  (int i = 0; i < n; i++) {
+            players[0][i] = i+1;
+            players[1][i] = 0;
+        }
+        int[] current = new int [n];
 
-        while (number < 7) {
+        int winner = 0;
+        int max = 0;
+        int flag = 0;
+
+        final Random random = new Random();
+
+        while (flag == 0) {
             for (int i = 0; i < n; i++){
-                players[i] += Math.random() %(n*k); // надо поменять функцию
+                for (int j = 0; j < k; j++) {
+                    
+                    current[i] += random.nextInt(5) + 1; 
+                }
+                if ( current[i] > max) {
+                    max = current[i];
+                    winner = i;
+                }
             };
+            players[1][winner]++;
+            if (players[1][winner]==7){
+                flag = 1;
+            } 
+            else {
+                int number = players[1][winner];
+                for(int i = winner; i > 0; i--) {
+                    players[0][i] = players[0][i-1];
+                    players[1][i] = players[1][i-1];
+                }
+                players[0][0] = winner+1;
+                players[1][0] = number;
+                for (int i=0; i < n; i++) {
+                    current[i] = 0;
+                }
+
+            }
         };
+
+        System.out.println("The winner is " + players[0][0] + " player");
 
     };
 }
