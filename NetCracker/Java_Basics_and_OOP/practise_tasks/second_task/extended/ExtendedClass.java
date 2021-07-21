4505ac2a058451045cf200e1ff5bf15a3d64c7e7
julproh <cherry5005@mail.ru>
2021-07-21 23:32:43 +0300

    modified:   NetCracker/Java_Basics_and_OOP/practise_tasks/first_task/report/report.aux 	modified:   NetCracker/Java_Basics_and_OOP/practise_tasks/first_task/report/report.fdb_latexmk 	modified:   NetCracker/Java_Basics_and_OOP/practise_tasks/first_task/report/report.fls 	modified:   NetCracker/Java_Basics_and_OOP/practise_tasks/first_task/report/report.log 	deleted:    NetCracker/Java_Basics_and_OOP/practise_tasks/first_task/report/report.out 	deleted:    NetCracker/Java_Basics_and_OOP/practise_tasks/first_task/report/report.synctex.gz 	modified:   NetCracker/Java_Basics_and_OOP/practise_tasks/first_task/report/report.tex 	deleted:    NetCracker/Java_Basics_and_OOP/practise_tasks/first_task/report/report.toc 	modified:   NetCracker/Java_Basics_and_OOP/practise_tasks/second_task/extended/ExtendedClass.java 	NetCracker/Java_Basics_and_OOP/practise_tasks/second_task/black/ 	NetCracker/Java_Basics_and_OOP/practise_tasks/second_task/extended.zip 	NetCracker/Java_Basics_and_OOP/practise_tasks/second_task/extended/ExtendedClass.class 	NetCracker/Java_Basics_and_OOP/practise_tasks/second_task/extended/Main.class 	NetCracker/Java_Basics_and_OOP/practise_tasks/second_task/extended/Main.java 	NetCracker/Java_Basics_and_OOP/practise_tasks/second_task/voice/report.tex

--- NetCracker/Java_Basics_and_OOP/practise_tasks/second_task/extended/ExtendedClass.java
+++ NetCracker/Java_Basics_and_OOP/practise_tasks/second_task/extended/ExtendedClass.java
@@ -1,10 +1,40 @@
-public class ExtandedClass {
+import java.util.Objects;
+
+public class ExtendedClass {
     byte b;
     int i;
     double d;
     String s;
 
-    boolean equals(Object anObject) {
-        return (this.b == anObject.byte, this.i == anObject.int, this.d == anObject.double, this.String == anObject.String) : true ? false;
-    
+    @Override
+    public boolean equals(Object anObject) {
+        if (anObject == this) {
+            return true;
+        } else if (anObject == null || anObject.getClass() != this.getClass()) {
+            return false;
+        }
+
+        ExtendedClass extendedClass = (ExtendedClass) anObject;
+        if (this.b == extendedClass.b && this.i  == extendedClass.i && this.d == extendedClass.d && this.s == extendedClass.s) {
+            return true;
+        } 
+        else {
+            return false;
+        }    
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(b);
+        result = 31 * result + Objects.hashCode(i);
+        result = 31 * result + Objects.hashCode(d);
+        result = 31 * result + Objects.hashCode(s);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+
+        return "byte: "+ b + "\n" + "int: " +"i "+ "\n" + "double: " + d + "\n" + "String: " + s ;
+    }
 }
