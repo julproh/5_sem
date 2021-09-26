fc8a3cb2060a62a31cf9900c304eb8cf3d3c0b66
julproh <cherry5005@mail.ru>
2021-09-26 18:23:51 +0300

    Added new file

---
+++ NetCracker/spring_boot/src/main/java/com/example/spring_boot/repo/PostRepository.java
@@ -0,0 +1,9 @@
+package com.example.spring_boot.repo;
+
+import com.example.spring_boot.models.Post;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends CrudRepository<Post, Long> {
+}
